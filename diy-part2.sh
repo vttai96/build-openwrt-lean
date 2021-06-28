712e8134c64f9e6173cccabe06c9bbb1b8149f39
vttai96 <52731691+vttai96@users.noreply.github.com>
2021-06-28 23:03:51 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,9 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+sed -i 's/OpenWrt/newifi3/g' package/base-files/files/bin/config_generate
