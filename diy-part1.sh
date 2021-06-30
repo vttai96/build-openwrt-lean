8f5d14a20015ec8218238ff5ce7dc96f1cc3deb9
vttai96 <52731691+vttai96@users.noreply.github.com>
2021-06-30 10:47:51 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git lienol https://github.com/Hyy2001X/luci-app-adguardhome' feeds.conf.default
-sed -i '$a src-git lienol https://github.com/rufengsuixing/luci-app-usb3disable' feeds.conf.default
+sed -i '$a src-git agh https://github.com/Hyy2001X/luci-app-adguardhome' feeds.conf.default
+sed -i '$a src-git usb3 https://github.com/rufengsuixing/luci-app-usb3disable' feeds.conf.default
