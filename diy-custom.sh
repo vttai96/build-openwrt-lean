c2312408aaeb941167a07cde44e859429647c42f
vttai96 <52731691+vttai96@users.noreply.github.com>
2021-07-14 13:08:24 +0700

    Update diy-custom.sh

--- diy-custom.sh
+++ diy-custom.sh
@@ -5,9 +5,9 @@ sed -i 's/https\:\/\/api\.github\.com\/repos\/AdguardTeam\/AdGuardHome\/releases
 sed -i 's/\+@LUCI_LANG_zh\-cn/\+@LUCI_LANG_zh\-cn \+@LUCI_LANG_en/g' package/lean/default-settings/Makefile
 
 # Modify default language, timezone
-sed -i 's/uci set luci\.main\.lang\=zh_cn/uci set luci\.main\.lang\=en/g' /package/lean/default-settings/files/zzz-default-settings
+sed -i 's/uci set luci\.main\.lang\=zh_cn/uci set luci\.main\.lang\=en/g' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/uci set system\.@system\[0\]\.timezone\=CST\-8/uci set system\.@system\[0\]\.timezone\='\<\+07\>\-7'/g' package/lean/default-settings/files/zzz-default-settings
 sed -i "s/uci set system\.@system\[0\]\.zonename\=Asia\/Shanghai/uci set system\.@system\[0\]\.zonename\='Asia\/Ho Chi Minh'/g" package/lean/default-settings/files/zzz-default-settings
 
 # Modify luci default language
-sed -i 's/uci set luci\.main\.lang\=zh_cn/uci set luci\.main\.lang\=en/g' feed/luci/modules/luci-base/root/etc/uci-defaults/luci-base
+sed -i 's/uci set luci\.main\.lang\=zh_cn/uci set luci\.main\.lang\=en/g' feeds/luci/modules/luci-base/root/etc/uci-defaults/luci-base
